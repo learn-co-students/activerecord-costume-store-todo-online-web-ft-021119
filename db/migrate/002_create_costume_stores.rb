079bfd62c9303bc160fb75f4f0d588ad431c2c64
Christoph Gruener <christophgruener@me.com>
2019-03-25 19:07:03 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       cs.integer :costume_inventory
       cs.integer :num_of_employees
       cs.boolean :still_in_business
-      cs.float :opening_time
-      cs.float :closing_time
+      cs.datetime :opening_time
+      cs.datetime :closing_time
     end
   end
 
