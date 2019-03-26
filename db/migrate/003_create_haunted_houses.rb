f2050ddabed214fa68d8c400062cc5f2ba8d8deb
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-26 15:24:45 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-  create_table :costume_stores do |t|
+  create_table :haunted_houses do |t|
     t.string :name
     t.string :location
     t.string :theme
