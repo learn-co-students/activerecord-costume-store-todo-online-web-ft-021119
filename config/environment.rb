c9fdb4cae284dbaf529d12265331b68ae456bdb8
Jacqi Entzminger <jackie.entz@gmail.com>
2019-04-01 05:29:33 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -18,3 +18,9 @@ DB = ActiveRecord::Base.connection
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
+
+# sql =<<-SQL
+#   CREATE TABLE IF NOT EXISTS costumes(
+#   id INTEGER PRIMARY KEY, name TEXT)
+#   SQL 
+#   ActiveRecord::Base.connection.execute(sql)
